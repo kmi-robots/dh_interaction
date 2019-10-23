40376661a5061c9be335398800a2d1b7a1f17ab0
Hi-Zed <gianluca.bardaro@gmail.com>
2019-10-23 16:22:02 +0100

    simple waypoint navigation

---
+++ scripts/navigation_exploration.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+from tf.transformations import random_quaternion
+import requests
+from requests.auth import HTTPBasicAuth
+import datetime
+import actionlib
+from geometry_msgs.msg import Twist
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import PoseStamped, Quaternion
+from std_srvs.srv import SetBool
+
+teamid = "kmirobots"
+teamkey = "0e920c05-e7a0-4745-9e94-eff7e1343b5d"
+
+waypoints = [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0]]
+
+if __name__ == '__main__':
+    rospy.init_node("simple_navigation_exploration")
+
+    pub_vel = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
+    trigger_client = rospy.ServiceProxy("/start_exploration", SetBool)
+    nav_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    rospy.loginfo("waiting for action server")
+    nav_client.wait_for_server()
+
+    rospy.loginfo("node started")
+
+    for w in waypoints:
+        rospy.loginfo("send goal")
+        goal_pose = MoveBaseGoal()
+        goal_pose.target_pose.header.frame_id = "/map"
+        goal_pose.target_pose.header.stamp = rospy.Time.now()
+        goal_pose.target_pose.pose.position.x = w[0]
+        goal_pose.target_pose.pose.position.y = w[1]
+        q = random_quaternion()
+        goal_pose.target_pose.pose.orientation = Quaternion(q[0], q[1], q[2], q[3])
+        nav_client.send_goal(goal_pose)
+        nav_client.wait_for_result()
+
+        t0 = rospy.Time.now()
+
+        rospy.loginfo("starting exploration")
+        trigger_client(True)
+        while rospy.Time.now() - t0 < rospy.Duration(10):
+            cmd = Twist()
+            cmd.angular.z = 0.3
+            rospy.sleep(0.1)
+        trigger_client(False)
+
+
+
--- scripts/send_pose.py
+++ scripts/send_pose.py
@@ -8,16 +8,18 @@ import datetime
 
 teamid = "kmirobots"
 teamkey = "0e920c05-e7a0-4745-9e94-eff7e1343b5d"
+pose_url = ""
 
 url = "https://api.mksmart.org/sciroc-competition/kmirobots/sciroc-robot-location/"
+
 
 def query_pose(event):
     global pose_url
     try:
-        (trans,rot) = listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
+        (trans, rot) = listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
         
-        t = datetime.datetime.fromtimestamp(rospy.Time.now().secs)
-        timestamp = t.strftime("%Y-%m-%dT%H:%M:%SZ")
+        t0 = datetime.datetime.fromtimestamp(rospy.Time.now().secs)
+        timestamp = t0.strftime("%Y-%m-%dT%H:%M:%SZ")
         
         payload = "  {\n    \"@id\": \""+pose_id+"\",\n    \"@type\": \"RobotLocation\",\n    \"episode\": \"EPISODE12\",\n    \"team\": \"kmirobots\",\n    \"timestamp\": \""+timestamp+"\",\n    \"x\": "+str(trans[0])+",\n    \"y\": "+str(trans[1])+",\n    \"z\": "+str(trans[2])+"\n}"
         rospy.loginfo(payload)
@@ -28,7 +30,8 @@ def query_pose(event):
         rospy.loginfo(response.text)
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
         pass
-    
+
+
 if __name__ == '__main__':
     
     rospy.init_node('send_pose')
