58675c715a7a4b7968575f4316959e178d13762d
Hi-Zed <gianluca.bardaro@gmail.com>
2019-10-23 14:58:57 +0100

    first commit

---
+++ scripts/send_pose.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python  
+
+import rospy
+import tf
+import requests
+from requests.auth import HTTPBasicAuth
+import datetime
+
+teamid = "kmirobots"
+teamkey = "0e920c05-e7a0-4745-9e94-eff7e1343b5d"
+
+url = "https://api.mksmart.org/sciroc-competition/kmirobots/sciroc-robot-location/"
+
+def query_pose(event):
+    global pose_url
+    try:
+        (trans,rot) = listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
+        
+        t = datetime.datetime.fromtimestamp(rospy.Time.now().secs)
+        timestamp = t.strftime("%Y-%m-%dT%H:%M:%SZ")
+        
+        payload = "  {\n    \"@id\": \""+pose_id+"\",\n    \"@type\": \"RobotLocation\",\n    \"episode\": \"EPISODE12\",\n    \"team\": \"kmirobots\",\n    \"timestamp\": \""+timestamp+"\",\n    \"x\": "+str(trans[0])+",\n    \"y\": "+str(trans[1])+",\n    \"z\": "+str(trans[2])+"\n}"
+        rospy.loginfo(payload)
+        
+        complete_url = url + pose_id
+        
+        response = requests.request("POST", complete_url, data=payload, auth=HTTPBasicAuth(teamkey, ''))
+        rospy.loginfo(response.text)
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        pass
+    
+if __name__ == '__main__':
+    
+    rospy.init_node('send_pose')
+    
+    t = rospy.Time.now()
+    pose_id = "pose_"+str(t.secs)+"_"+str(t.nsecs)
+
+    listener = tf.TransformListener()
+    
+    rospy.Timer(rospy.Duration(1), query_pose)
+    
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        rate.sleep()
